f5afcd295a4e3559ef78c992a55a1004d8226792
who_what_ <gabbyvirusky2004@gmail.com>
2024-08-31 14:23:16 +0000

    Added components on earn page

--- src/Pages/Earn.tsx
+++ src/Pages/Earn.tsx
@@ -1,18 +1,123 @@
 import Coin from '../images/dollar-coin.png';
+import Arrow from '../images/Arrow.png';
+import Checkmark from '../images/Checkmark.png';
+import ReactIcon from '../assets/react.svg';
 
 const Earn = () => {
 
 
 
     return (
-      <div className="flex flex-col font-sans h-screen bg-gradient-to-b from-[#185C8D] to-[#1A1F20] text-white p-4 ">
+      <div className="flex flex-col font-sans pb-16 bg-gradient-to-b from-[#185C8D] to-[#1A1F20] text-white p-4 ">
         <h1 className='text-center font-bold font-sans text-2xl'>
           Bamboo
         </h1>
         <div className='p-8 flex justify-center'>
           <img src={Coin}/>
         </div>
-        <p className='text-center font-bold font-sans text-2xl'>Boost your mine rate</p>
+        <p className='text-center font-bold font-sans mb-6 text-2xl'>Boost your mine rate</p>
+        <div className='mb-2'>
+          <p >Tier 1 rewards</p>
+          <div className="bg-gray-700 justify-left mb-2 flex flex-row rounded-xl p-3 text-sm pl-8 my-auto">
+          <img src={ReactIcon} className="w-6 h-6 my-auto mr-4" /> 
+          <div className=''>
+            <div className="flex my-auto flex-col mr-4">Join our Telegram Channel</div>
+            <div className='flex flex-row'>
+              <img src={Coin} className="my-auto mr-1 w-4 h-4" />
+              <p className='text-yellow-200'>+1234</p> 
+              <p className='ml-1 text-white'>per hour</p>
+            </div>
+          </div>
+          <img className='ml-40 my-auto h-6' src={Arrow}/>
+          </div>
+          <div className="bg-gray-700 justify-left mb-2 flex flex-row rounded-xl p-3 text-sm pl-8 my-auto">
+          <img src={ReactIcon} className="w-6 h-6 my-auto mr-4" /> 
+          <div className=''>
+            <div className="flex my-auto flex-col mr-4">Join our Telegram Channel</div>
+            <div className='flex flex-row'>
+              <img src={Coin} className="my-auto mr-1 w-4 h-4" />
+              <p className='text-yellow-200'>+1234</p> 
+              <p className='ml-1 text-white'>per hour</p>
+            </div>
+          </div>
+          <img className='ml-40 my-auto h-6' src={Checkmark}/>
+          </div>
+          <div className="bg-gray-700 justify-left mb-2 flex flex-row rounded-xl p-3 text-sm pl-8 my-auto">
+          <img src={ReactIcon} className="w-6 h-6 my-auto mr-4" /> 
+          <div className=''>
+            <div className="flex my-auto flex-col mr-4">Join our Telegram Channel</div>
+            <div className='flex flex-row'>
+              <img src={Coin} className="my-auto mr-1 w-4 h-4" />
+              <p className='text-yellow-200'>+1234</p> 
+              <p className='ml-1 text-white'>per hour</p>
+            </div>
+          </div>
+          <img className='ml-40 my-auto h-6' src={Arrow}/>
+          </div>
+        </div>
+        <div>
+          <p>Tier 2 rewards</p>
+          <div className="bg-gray-700 justify-left mb-2 flex flex-row rounded-xl p-3 text-sm pl-8 my-auto">
+          <img src={ReactIcon} className="w-6 h-6 my-auto mr-4" /> 
+          <div className=''>
+            <div className="flex my-auto flex-col mr-4">Join our Telegram Channel</div>
+            <div className='flex flex-row'>
+              <img src={Coin} className="my-auto mr-1 w-4 h-4" />
+              <p className='text-yellow-200'>+1234</p> 
+              <p className='ml-1 text-white'>per hour</p>
+            </div>
+          </div>
+          <img className='ml-40 my-auto h-6' src={Arrow}/>
+          </div>
+          <div className="bg-gray-700 justify-left mb-2 flex flex-row rounded-xl p-3 text-sm pl-8 my-auto">
+          <img src={ReactIcon} className="w-6 h-6 my-auto mr-4" /> 
+          <div className=''>
+            <div className="flex my-auto flex-col mr-4">Join our Telegram Channel</div>
+            <div className='flex flex-row'>
+              <img src={Coin} className="my-auto mr-1 w-4 h-4" />
+              <p className='text-yellow-200'>+1234</p> 
+              <p className='ml-1 text-white'>per hour</p>
+            </div>
+          </div>
+          <img className='ml-40 my-auto h-6' src={Arrow}/>
+          </div>
+          <div className="bg-gray-700 justify-left mb-2 flex flex-row rounded-xl p-3 text-sm pl-8 my-auto">
+          <img src={ReactIcon} className="w-6 h-6 my-auto mr-4" /> 
+          <div className=''>
+            <div className="flex my-auto flex-col mr-4">Join our Telegram Channel</div>
+            <div className='flex flex-row'>
+              <img src={Coin} className="my-auto mr-1 w-4 h-4" />
+              <p className='text-yellow-200'>+1234</p> 
+              <p className='ml-1 text-white'>per hour</p>
+            </div>
+          </div>
+          <img className='ml-40 my-auto h-6' src={Arrow}/>
+          </div>
+          <div className="bg-gray-700 justify-left mb-2 flex flex-row rounded-xl p-3 text-sm pl-8 my-auto">
+          <img src={ReactIcon} className="w-6 h-6 my-auto mr-4" /> 
+          <div className=''>
+            <div className="flex my-auto flex-col mr-4">Join our Telegram Channel</div>
+            <div className='flex flex-row'>
+              <img src={Coin} className="my-auto mr-1 w-4 h-4" />
+              <p className='text-yellow-200'>+1234</p> 
+              <p className='ml-1 text-white'>per hour</p>
+            </div>
+          </div>
+          <img className='ml-40 my-auto h-6' src={Arrow}/>
+          </div>
+          <div className="bg-gray-700 justify-left mb-2 flex flex-row rounded-xl p-3 text-sm pl-8 my-auto">
+          <img src={ReactIcon} className="w-6 h-6 my-auto mr-4" /> 
+          <div className=''>
+            <div className="flex my-auto flex-col mr-4">Join our Telegram Channel</div>
+            <div className='flex flex-row'>
+              <img src={Coin} className="my-auto mr-1 w-4 h-4" />
+              <p className='text-yellow-200'>+1234</p> 
+              <p className='ml-1 text-white'>per hour</p>
+            </div>
+          </div>
+          <img className='ml-40 my-auto h-6' src={Arrow}/>
+          </div>
+        </div>
       </div>
     );
   };
--- src/Pages/Friends.tsx
+++ src/Pages/Friends.tsx
@@ -1,3 +1,7 @@
+import Coin from '../images/dollar-coin.png';
+import Arrow from '../images/Arrow.png';
+import Checkmark from '../images/Checkmark.png';
+import ReactIcon from '../assets/react.svg';
 
 const Friends = () => {
 
@@ -8,6 +12,9 @@ const Friends = () => {
     <h1 className='text-center font-bold font-sans text-2xl'>
       Bamboo
     </h1>
+    
+
+
     </div>
   );
 };
