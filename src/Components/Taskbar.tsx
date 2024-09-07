8493feae6df658901eab3940b3ee6359718a8ca4
who_what_ <gabbyvirusky2004@gmail.com>
2024-09-07 16:06:39 +0000

    removed taskbar trasnparency

--- src/Components/Taskbar.tsx
+++ src/Components/Taskbar.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Taskbar = () => {
   return (
     <div className="taskbar">
-      <div className='p-4 bg-opacity-95 pt-2 pb-2 h-16 bg-[#32363C] rounded-lg justify-between flex flex-row'>
+      <div className='p-4 pt-2 pb-2 h-16 bg-[#32363C] rounded-lg justify-between flex flex-row'>
         <Link to="/" className="bg-[#212429] pl-2 w-16 pr-2 justify-center rounded-md text-center text-xs flex flex-col">
           <img src={binanceLogo} className="w-6 mx-auto h-5" alt="Home Logo" /> Home
         </Link>
