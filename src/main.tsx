f7bff6dbaf2590a8870497a817a5ba2840349fa7
who_what_ <gabbyvirusky2004@gmail.com>
2024-09-23 15:45:20 +0000

    Assembled successfully

--- src/main.tsx
+++ src/main.tsx
@@ -6,55 +6,19 @@ import Airdrop from './Pages/Airdrop.tsx';
 import Earn from './Pages/Earn.tsx';
 import Friends from './Pages/Friends.tsx';
 import './index.css';
-import Layout from './layout.tsx'; // Import the new Layout component
-import { supabase } from './client.ts';
-
-const initTelegramAuth = () => {
-  // Ensure Telegram.WebApp is defined
-  if (window.Telegram && window.Telegram.WebApp) {
-    const webApp = window.Telegram.WebApp;
-
-    webApp.expand();
-    webApp.onEvent('auth', function() {
-      const userData = webApp.initDataUnsafe;
-
-      fetch('/api/telegram-login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(userData)
-      })
-      .then(response => response.json())
-      .then(data => {
-        const token = data.token;
-        supabase.auth.setSession(token)
-          .then((response) => {
-            if (response.error) {
-              console.error('Error setting session:', response.error);
-            } else {
-              console.log('User logged in successfully');
-            }
-          });
-      })
-      .catch(error => console.error('Error:', error));
-    });
-  } else {
-    console.error('Telegram WebApp is not loaded');
-  }
-};
-
-initTelegramAuth();
+import Taskbar from './Components/Taskbar';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/airdrop" element={<Airdrop />} />
-          <Route path="/earn" element={<Earn />} />
-          <Route path="/friends" element={<Friends />} />
-        </Routes>
-      </Layout>
+      <Taskbar />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/airdrop" element={<Airdrop />} />
+        <Route path="/earn" element={<Earn />} />
+        <Route path="/friends" element={<Friends />} />
+      </Routes>
     </Router>
   </React.StrictMode>
 );
+
