26938bad2554504227e985c725f37b65c75c6f98
who_what_ <gabbyvirusky2004@gmail.com>
2024-10-07 18:05:11 +0000

    Conditional styling of taskbar icons

--- src/Components/Taskbar.tsx
+++ src/Components/Taskbar.tsx
@@ -1,6 +1,10 @@
+import HomeOpen from '../icons/HomeOpen.svg';
+import AirdropOpen from '../icons/Group.svg';
+import Friends from '../icons/Friends.svg';
+import EarnOpen from '../icons/Vector.svg';
 import Home from '../icons/teenyicons_home-solid.svg';
 import Earn from '../icons/ph_coins-fill.svg';
-import Friends from '../icons/fa-solid_user-friends.svg';
+import FriendsOpen from '../icons/fa-solid_user-friends.svg';
 import Airdrop from '../icons/mingcute_airdrop-fill.svg';
 import { NavLink } from 'react-router-dom';
 
@@ -8,60 +12,89 @@ const Taskbar = () => {
   return (
     <div className="taskbar">
       <div className="p-4 bg-[#3D4549] pt-2 pb-2 h-16 justify-between flex flex-row">
+        
+        {/* Home */}
         <NavLink
           to="/"
           className={({ isActive }) =>
-            `pl-2 w-16 pr-2 justify-center text-center text-xs flex flex-col ${isActive ? 'text-green-500' : 'text-gray-500'}`
+            `pl-2 w-16 pr-2 justify-center text-center text-xs flex flex-col ${
+              isActive ? 'text-[#FFD700]' : 'text-white'
+            }`
           }
         >
-          <img
-            src={Home}
-            className="w-6 mx-auto h-5"
-            alt="Home Logo"
-          />
-          Home
+          {({ isActive }) => (
+            <>
+              <img
+                src={isActive ? HomeOpen : Home}
+                className="w-6 mx-auto h-5"
+                alt="Home Logo"
+              />
+              Home
+            </>
+          )}
         </NavLink>
 
+        {/* Earn */}
         <NavLink
           to="/earn"
           className={({ isActive }) =>
-            `pl-2 w-16 pr-2 justify-center text-center text-xs flex flex-col ${isActive ? 'text-green-500' : 'text-gray-500'}`
+            `pl-2 w-16 pr-2 justify-center text-center text-xs flex flex-col ${
+              isActive ? 'text-[#FFD700]' : 'text-white'
+            }`
           }
         >
-          <img
-            src={Earn}
-            className="w-6 mx-auto h-5"
-            alt="Earn Logo"
-          />
-          Earn
+          {({ isActive }) => (
+            <>
+              <img
+                src={isActive ? EarnOpen : Earn}
+                className="w-6 mx-auto h-5"
+                alt="Earn Logo"
+              />
+              Earn
+            </>
+          )}
         </NavLink>
 
+        {/* Friends */}
         <NavLink
           to="/friends"
           className={({ isActive }) =>
-            `pl-2 w-16 pr-2 justify-center text-center text-xs flex flex-col ${isActive ? 'text-green-500' : 'text-gray-500'}`
+            `pl-2 w-16 pr-2 justify-center text-center text-xs flex flex-col ${
+              isActive ? 'text-[#FFD700]' : 'text-white'
+            }`
           }
         >
-          <img
-            src={Friends}
-            className="w-6 mx-auto h-5"
-            alt="Friends Logo"
-          />
-          Friends
+          {({ isActive }) => (
+            <>
+              <img
+                src={isActive ? FriendsOpen : Friends}
+                className="w-6 mx-auto h-5"
+                alt="Friends Logo"
+              />
+              Friends
+            </>
+          )}
         </NavLink>
 
+        {/* Airdrop */}
         <NavLink
           to="/airdrop"
           className={({ isActive }) =>
-            `pl-2 w-16 pr-2 justify-center text-center text-xs flex flex-col ${isActive ? 'text-green-500' : 'text-gray-500'}`
+            `pl-2 w-16 pr-2 justify-center text-center text-xs flex flex-col ${
+              isActive ? 'text-[#FFD700]' : 'text-white'
+            }`
           }
         >
-          <img
-            src={Airdrop}
-            className="w-6 fill-[#E5D7D7] mx-auto bg-blue h-5"
-            alt="Airdrop Logo"
-          />
-          Airdrop
+          {({ isActive }) => (
+            <>
+              <img
+                src={isActive ? AirdropOpen : Airdrop}
+                className="w-6 mx-auto h-5"
+                alt="Airdrop Logo"
+              />
+              Airdrop
+            </>
+          )}
         </NavLink>
       </div>
     </div>
