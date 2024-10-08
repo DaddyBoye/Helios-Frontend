1559964681f0340594af05e278c66d5d334ff78d
who_what_ <gabbyvirusky2004@gmail.com>
2024-10-08 23:56:25 +0000

    Styled Earn page header

--- src/Pages/Earn.tsx
+++ src/Pages/Earn.tsx
@@ -3,16 +3,39 @@ import Minions from '../images/napoleon-exile-gettyimages-1288489073.jpg';
 import Instagram from '../icons/insta-removebg-preview 1.svg';
 import X from '../icons/x-removebg-preview 1.svg';
 import YouTube from '../icons/yt-removebg-preview 1.svg';
-import Telegram from '../icons/tg-removebg-preview 1.svg'
-
-
+import Telegram from '../icons/tg-removebg-preview 1.svg';
+import Solis from '../icons/fdv 1.svg';
+import Friends from '../icons/Friends Vector.svg';
+import User from '../icons/edeef 1.svg'
 
 const Earn = () => {
+
+
     return (
         <div className="relative font-sans h-full pb-20 flex flex-col overflow-y-auto">
             <StarryBackground />
             <div className="relative z-10 flex flex-col text-center text-white flex-grow">
-                <p className='text-left text-2xl pb-4 pt-6 pl-3'>Earn</p>
+                <div className='flex flex-row justify-between mt-4 m-2 bg-[#185C8D]/50 h-14 p-2 pl-3 rounded-lg'>
+                    <div className='flex flex-row items-center justify-center'>
+                        <img src={Solis} alt="" width={36} height={36} />
+                        <p className='text-lg'>Earn</p>
+                    </div>
+            <div className="relative flex my-auto h-7">
+                <div className="flex flex-row justify-between gap-5 rounded-l-2xl rounded-r-2xl pl-4 pr-16 p-1 bg-[#185C8D]/80">
+                    <div className="flex flex-row items-center">
+                        <img src={Solis} alt="" className="w-6 h-6" />
+                        <p className="ml-1">10</p>
+                    </div>
+                    <div className="flex flex-row items-center">
+                        <img src={Friends} alt="" className="w-6 h-6" />
+                        <p className="ml-1">20</p>
+                    </div>
+                </div>
+                <div className="absolute right-0 top-1/2 -translate-y-1/2 bg-[#FFD700] rounded-full p-2 flex items-center justify-center">
+                    <img src={User} alt="" className="w-8 h-8 rounded-full object-cover" />
+                </div>
+        </div>
+            </div>
                 <div className='flex gap-2 pl-3 overflow-x-auto pr-3 hide-scrollbar'>
                     <div className='w-10/12 max-h-50 rounded-2xl min-h-40 shrink-0 relative'>
                         {/* Image with fade effect */}
