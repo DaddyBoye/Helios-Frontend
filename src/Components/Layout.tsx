ea6c15d2f294be64adfe0d98d13c96afd0d92abb
who_what_ <gabbyvirusky2004@gmail.com>
2024-11-08 19:58:26 +0000

    Refactor Layout component to enhance loading logic by ensuring all necessary data is fetched before stopping the loading state and update welcome page condition for new users

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -404,7 +404,7 @@ useEffect(() => {
   };
 
   useEffect(() => {
-    if (loadingTimePassed ) {
+    if (loadingTimePassed && dataFetched && heliosUsernameFetched && avatarPathFetched) {
       setIsLoading(false);  // Loading stops once all essential and additional data is fetched (or attempted)
     }
   }, [loadingTimePassed, user, dataFetched, heliosUsernameFetched, avatarPathFetched]);
@@ -441,7 +441,7 @@ useEffect(() => {
     return <LoadingPage />;
   }
 
-  if (newUser === false) {
+  if (newUser === true || newUser === null) {
     return showWelcomePage ? (
       <WelcomePage onContinue={() => handleSetWelcomePage(false)} /> // Welcome page shown first
     ) : (
