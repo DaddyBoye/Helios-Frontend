0714ffe1e4c1f978374f3548eeeb1d47592770e3
Gabriel Adjei Boye <gabrieladjeiboye@gmail.com>
2025-01-28 15:23:27 +0000

    Adjust positioning of "Claim Now" button in Earn page for improved layout

--- src/Pages/Earn.tsx
+++ src/Pages/Earn.tsx
@@ -459,7 +459,7 @@ const renderInviteTasksSection = () => (
                             </div>
                         )}
                         {claimable && !completed && (
-                            <div className="absolute top-4 right-2 -mr-1 bg-yellow-500 text-white text-xs px-2 py-1 rounded-full">
+                            <div className="absolute top-4 ml-2 right-2 -mr-1 bg-yellow-500 text-white text-xs px-2 py-1 rounded-full">
                                 Claim Now
                             </div>
                         )}
